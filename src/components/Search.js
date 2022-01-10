46a99e6c21d24c80022cea20651612a5427f7353
Alberto Camacho Sampedro <acamacho.tech@gmail.com>
2022-01-10 12:22:49 +0100

    Handle events in react

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,13 @@
 export default function Search() {
+  
+  function handleChange(event) {
+    console.log(event.target.value);
+  }
+
   return (
     <>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <input id="search" type="text" onChange={handleChange}  />
     </>
   );
 }
